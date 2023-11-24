63f354282908f29d1db01f73a9e85d69e8732bac
dsi <dsi28@hotmail.com>
2023-11-24 12:58:48 -0600

    add logg out

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return <div>Hello {value}!
   
   <div><a href="/profile">profile</a></div>
-  <a href="/.auth/login/github">Login</a>
-  <a href="/.auth/login/github?post_login_redirect_uri=https://http://localhost:3000/profile">Login</a>
+  <a href="/.auth/login/aad?post_login_redirect_uri=https://ambitious-moss-01e0b2f0f.4.azurestaticapps.net/profile">Login</a>
   </div>;
 }
 
